be91dbd2e29a4c36e30c1998282c514e4989bd3d
sugarshin <shinsugar@gmail.com>
2018-01-24 19:01:05 +0900

    Patch number emoji exception to Emoji component

--- draft-js-emoji-plugin/src/components/Emoji/index.js
+++ draft-js-emoji-plugin/src/components/Emoji/index.js
@@ -16,8 +16,14 @@ const Emoji = ({ theme = {}, cacheBustParam, imagePath, imageType, className, de
     );
   } else {
     // short name to image url code steal from emojione source code
-    const shortNameForImage = emojione.emojioneList[shortName].unicode[emojione.emojioneList[shortName].unicode.length - 1];
-    const backgroundImage = `url(${imagePath}${shortNameForImage}.${imageType}${cacheBustParam})`;
+    // TODO: patch for https://github.com/draft-js-plugins/draft-js-plugins/issues/717
+    // const shortNameForImage = emojione.emojioneList[shortName].unicode[emojione.emojioneList[shortName].unicode.length - 1];
+    // const backgroundImage = `url(${imagePath}${shortNameForImage}.${imageType}${cacheBustParam})`;
+    const { unicode } = emojione.emojioneList[shortName] || { unicode: [] };
+    const shortNameForImage = unicode[unicode.length - 1];
+    const backgroundImage = shortNameForImage ?
+      `url(${imagePath}${shortNameForImage}.${imageType}${cacheBustParam})` : undefined;
+
     const combinedClassName = unionClassNames(theme.emoji, className);
 
     emojiDisplay = (
